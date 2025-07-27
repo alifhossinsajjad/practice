de22a7f9d228ae4a2f135c5b1c63d69673ec3658
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-27 23:26:14 +0600

    add a big ammout and substraction 2 fruts value and at last total cost subtraction fruits

---
+++ buy.js
@@ -0,0 +1,7 @@
+var totalMoney = 2000;
+var applePrice = 500;
+var orangePrice = 850;
+var fruitesPrice = ( applePrice + orangePrice);
+var totalTaka = (totalMoney - fruitesPrice)
+
+console.log(totalTaka);
