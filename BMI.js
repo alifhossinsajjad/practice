6f97a6dbf2e31b4c0a5637b11c57a0dec6c4ef29
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-28 21:53:40 +0600

    How to measure the MBI calculator

---
+++ BMI.js
@@ -0,0 +1,16 @@
+const BMI = 27.2;
+
+if (BMI < 18.5){
+    console.log('you are underweight')
+}
+else if (BMI <= 18.5 || BMI <= 24.9){
+    console.log(
+        'you are normal.'
+    )
+}
+else if (BMI <= 25 || BMI <= 29.9) {
+    console.log('You are overright')
+}
+else {
+    console.log('You are Obese')
+}
