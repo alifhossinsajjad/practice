4920110e77a11a48d2ead75e0c3832fd60f7a171
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-28 07:25:30 +0600

    added some comparisone

---
+++ comparisone.js
@@ -0,0 +1,20 @@
+console.log (5 > 10);
+console.log (2 < 5);
+console.log (45 == 599); 
+console.log (20 == 20);
+console.log (20 >= 12);
+console.log (5 >= 10);
+console.log (5 <= 20);
+console.log (10 <= 10);
+console.log (200 <= 30);
+
+console.log (20 == 20);
+console.log (400 == 400.2);
+console.log (200 === 200);
+
+
+var applePrice = 400;
+var orangePrice = 300;
+var totalPrice = (applePrice >= orangePrice);
+
+console.log (totalPrice);
