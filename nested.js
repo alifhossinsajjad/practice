fd9b0194c64c95a2e564b9dca6ace18c2ca7473f
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-07-28 09:48:15 +0600

    nested if-else cindition

---
+++ nested.js
@@ -0,0 +1,15 @@
+const money = 100;
+
+if (money >300){
+    console.log ('really bro you have lots of money')
+}
+else{
+    if(money >= 100){
+        console.log ('Then give me 100 taka. ')
+    }
+    else{
+        if(money > 0) {
+            console.log('Then I can not do anything.')
+        }
+    }
+}
