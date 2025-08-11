15e07bea2a1002bdfde43008d3b113763c719816
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-11 23:59:22 +0600

    how to diclare a validation problem

---
+++ js-problems-part-2/validation.js
@@ -0,0 +1,53 @@
+const { lstat } = require("fs");
+const { get } = require("http");
+const { resourceUsage } = require("process");
+
+function multiply (num1, num2){
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'Please provide a valid number'
+    }
+    const mult = num1 * num2;
+    return mult;
+}
+
+const result = multiply (4, 5)
+console.log(result); 
+
+
+
+
+
+function fullNmae (first , last){
+    if(typeof first !== 'string' || typeof last !== 'string'){
+        return 'Please prive a string'
+    }
+    const full = first + ' ' + last;
+    return full
+}
+
+const full = fullNmae('alif ', 'mia');
+console.log(full);
+
+
+function getPrice (product){
+    if(typeof product !== 'object'){
+        return 'please provide a objeact'
+    }
+    const price = product.price;
+    return price;
+}
+
+const price = getPrice({name : 'samsung', price : 30000, color : 'black'});
+console.log(price);
+
+
+function getSecond (numbers){
+    if( Array.isArray (numbers) === false){
+        return 'plase provide a valid array'
+    }
+    const second = numbers[1];
+    return second;
+}
+
+const second = getSecond([12, 34, 45]);
+console.log(second);
