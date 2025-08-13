17ce120e92013c9adf0a08133e481dcfd6b94d16
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-13 14:18:58 +0600

    how to find the largest word with using  sorted method

--- js-problems-part1-practice-tasks/task4.js
+++ js-problems-part1-practice-tasks/task4.js
@@ -16,3 +16,18 @@ function findLongestWord (str){
 
 const word = findLongestWord('I am learning Programming to become a programmer');
 console.log(word);
+
+
+
+
+// how to find the latgest word with sort method
+
+
+function findLogestChar (str){
+    const word = str.split(' ');
+    const sortedWords = word.sort((a, b) => b.length - a.length);
+    return sortedWords[0];
+}
+
+const longestWord = findLogestChar('I am learning Programming to become a programmer');
+console.log(longestWord);
