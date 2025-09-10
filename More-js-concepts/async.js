7fe2be9b8ea92a445f0189ffeb4a05896f1656f4
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-10 08:11:30 +0600

    how to learn more js concets

---
+++ More-js-concepts/async.js
@@ -0,0 +1,10 @@
+// syncronus
+
+console.log('1')
+console.log('2')
+console.log('i will be back after 10 second')
+setTimeout(() => {
+    console.log('3')
+}, 10000);
+console.log('4')
+console.log('5');
